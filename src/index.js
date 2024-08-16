de6a1e8a23aee618d7f2f0d6544a7d3c271667c8
showtech <showtechitconcept@gmail.com>
2024-08-16 10:28:01 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+//App
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//costom Style
+// import "./styles/w3.css";
+// import "./styles/index.scss";
+// import "./styles/small_screen.scss";
+// import "./styles/medium_screen.scss";
+// import "./styles/large_screen.scss";
+//===================================================
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const home=()=> {
+  return (
+    <div className="screen bg_color ">
+        
+        Home
+    
+    </div>
+  )
+}
+
+export default home
